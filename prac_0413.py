2d2a6085bdc4e21dc3bdaab02a7175ba4cec7572
ddaynew365 <64246267+ddaynew365@users.noreply.github.com>
2021-04-13 21:00:55 +0900

    Update prac_0413.py

--- prac_0413.py
+++ prac_0413.py
@@ -1,6 +1,7 @@
 /*
 프로그래머스 - 완주하지 못한 선수
 */
+#초기 코드
 
 def solution(participant, completion):
     com = sorted(completion)
@@ -13,9 +14,10 @@ def solution(participant, completion):
     answer = parti[0]
     return answer
   
-  /*
+/*
+결과는
   정확성: 50
   효율성: 10
    
-  확인 결과 효율성이 너무 부족하였다.
-    */
+  확인 결과 효율성이 너무 부족하였다. 어디를 고쳐야 효율성이 좋아질지 찾아봐야겠다
+ */
