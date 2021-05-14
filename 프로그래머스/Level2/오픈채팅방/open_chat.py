2bc3341f811365479c49dcb9d5747f870cf29468
이윤성 <64246267+ddaynew365@users.noreply.github.com>
2021-05-14 16:14:00 +0900

    open_chat.py 파일 수정
    수정본 내용 추가

--- 프로그래머스/Level2/오픈채팅방/open_chat.py
+++ 프로그래머스/Level2/오픈채팅방/open_chat.py
@@ -26,3 +26,30 @@ def solution(record):
 
 
     return answer
+
+'''
+수정본 -remove에 대한 조건식을 제거, 불필요한 리스트 제거, == 조건 보다 in 조건 비교가 속도가 더 빠르다
+'''
+def solution(record):
+    answer = []
+    dic_ = {}
+
+    for i in record:
+        com = i.split()
+
+        if com[0] in ['Enter','Change']:
+            dic_[com[1]] = com[2]
+
+
+
+    for i in record:
+        com = i.split()
+
+        if com[0] == 'Enter':
+            answer.append(dic_[com[1]]+'님이 들어왔습니다.')
+        elif com[0] == 'Leave':
+            answer.append(dic_[com[1]]+'님이 나갔습니다.')     
+
+
+
+    return answer
