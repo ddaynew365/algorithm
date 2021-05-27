25b16797fcd2f9098a0bc4e566f1276678106832
이윤성 <64246267+ddaynew365@users.noreply.github.com>
2021-05-27 17:31:20 +0900

    성공 - bfs 알고리즘 사용
    성공 - bfs 알고리즘 사용

--- 프로그래머스/Level3/가장 먼 노드/farthest_node.py
+++ 프로그래머스/Level3/가장 먼 노드/farthest_node.py
@@ -1,3 +1,4 @@
+# 가장 기본적인 다익스트라 알고리즘- 시간 복잡도가 O(V^2)으로 노드의 개수(V)가 5000개 이상만 되어도 걸리는 시간이 
 from collections import Counter
 INF = int(1e9)
 
@@ -58,6 +59,9 @@ def solution(n, edge):
   
   
 #==================================================================
+# 두 번째로 우선순위 큐를 사용한 다익스트라 알고리즘- 조금 더 시간 복잡도가 개선되었지만 문제에서 주어진 시간에 맞추질 못했다 
+#  생각해본 결과 weight가 모두 1인 상황에서는 다익스트라 알고리즘을 사용하는 것이 비효율적이라는 결론을 냈다.
+#  이런 상황에서는 bfs 알고리즘을 사용하는 것이 낫다고 판단하였다.
 from collections import Counter
 import heapq
 INF = int(1e9)
@@ -103,4 +107,37 @@ def solution(n, edge):
     count = dict(Counter(distance))
     answer = count[max_]
     
+    return answer
+
+#================================================================
+# 3 번쨰는 bfs 알고리즘을 사용하였다. 처음에는 주어진 edge 리스트를 그대로 사용해보았지만 그렇게 되면 bfs 알고리즘의 for문이 굉장히 비효율적으로 되었다
+# 따라서 graph라는 새로운 리스트를 만들어서 구현하였다. 그 결과 복잡한 테스트의 경우 20배 빠른 속도를 보여주었다.
+from collections import deque
+
+def bfs(n, graph, start):
+    distance = [-1] * (n+1)
+    distance[start] = 0
+    
+    queue = deque()
+    queue.append(start)
+    
+    while queue:
+        v = queue.popleft()
+        
+        for i in graph[v]:
+            if distance[i]  == -1:
+                distance[i] = distance[v]  + 1
+                queue.append(i)
+    return distance
+    
+def solution(n, edge):
+    answer = 0
+
+    graph = [ [] for _ in range(n + 1)]
+    for f, to in edge:
+        graph[f].append(to)
+        graph[to].append(f)
+        
+    distance = bfs(n, graph, 1)
+    answer = distance.count(max(distance))
     return answer
