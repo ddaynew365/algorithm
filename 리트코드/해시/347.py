c59825054ae1fafbfec3eb19cac1868e6af7a394
ddaynew365 <ddaynew365@gmail.com>
2022-01-18 18:33:22 +0900

    리트코드:347

---
+++ 리트코드/해시/347.py
@@ -0,0 +1,19 @@
+# 힙으로 푸는 방법=> Counter를 쓴 후 최대 힙에 숫자들을 집어놓고 순서대로 꺼낸다.
+from collections import Counter
+import heapq
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        result = []
+        heap = []
+        count = Counter(nums)
+        for num in count:
+            heapq.heappush(heap, (-count[num], num))
+        for i in range(k):
+            result.append(heapq.heappop(heap)[1])
+        return result
+
+# 파이썬틱한 방법 => zip은 리스트 여러개를 하나씩 꺼내서 새로운 리스트를 여러개 만드는 것, *는 unpack하는 방법
+from collections import Counter
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        return list(zip(*Counter(nums).most_common(k)))[0]
