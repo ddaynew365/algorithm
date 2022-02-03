83e6ed222dbc708454e09212a4b2cc9b76802601
ddaynew365 <ddaynew365@gmail.com>
2022-02-03 22:01:14 +0900

    리트코드:정렬 56 문제 풀이

---
+++ 리트코드/정렬/56.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        merged = list()
+        for start, end in sorted(intervals, key = lambda x:x[0]):
+            if merged and start <= merged[-1][1]:
+                merged[-1][1] = max(merged[-1][1], end)
+            else:
+                merged.append([start, end])
+                
+        return merged
