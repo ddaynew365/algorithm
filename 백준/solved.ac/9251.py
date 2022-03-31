8135b5a51477a4a57bb2681b683af81584f87434
ddaynew365 <ddaynew365@gmail.com>
2022-03-31 13:09:37 +0900

    백준:LCS

--- 백준/solved.ac/9251.py
+++ 백준/solved.ac/9251.py
@@ -1,22 +1,28 @@
-str1 = input()
-str2 = input()
-lcs = [[0 for _ in range(len(str2) + 1)] for _ in range(len(str1) + 1)]
+import sys
+input = sys.stdin.readline
+str1 = input().strip()
+str2 = input().strip()
 
-for i in range(len(str1) + 1):
-  for j in range(len(str2) + 1):
-    if i ==0 or j ==0:
-      continue
+n = len(str1)
+m = len(str2)
+lcs = [[0 for _ in range(m+1)] for _ in range(n+1)]
+
+for i in range(n+1):
+  for j in range(m+1):
+    if i == 0 or j == 0:
+      lcs[i][j] == 0
     elif str1[i-1] == str2[j-1]:
       lcs[i][j] = lcs[i-1][j-1] + 1
-    elif str1[i-1] != str2[j-1]:
+    else:
       lcs[i][j] = max(lcs[i-1][j], lcs[i][j-1])
-answer  = 0  
-for i in range(len(str1) + 1):
-  for j in range(len(str2)+ 1):
+
+answer = 0
+for i in range(n+1):
+  for j in range(m+1):
     answer = max(answer, lcs[i][j])
 print(answer)
-
-i, j = len(str1), len(str2)
+i = n
+j = m    
 result = []
 while lcs[i][j] != 0:
   if lcs[i][j] == lcs[i-1][j]:
@@ -24,7 +30,7 @@ while lcs[i][j] != 0:
   elif lcs[i][j] == lcs[i][j-1]:
     j = j-1
   else:
-    i,j = i-1, j-1
-    result.append(str1[i])
-
+    result.append(str1[i-1])
+    i, j = i-1,j-1
+    
 print(result[::-1])
